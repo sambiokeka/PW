5ea4a0a626588c3f45585022f9b1c750692dfee6
Erick <erickjooji@yahoo.com.br>
2024-08-28 16:33:59 -0300

    laranja bom dia

---
+++ laranjabomdia/src/app/layout.js
@@ -0,0 +1,13 @@
+import './globals.css';
+
+export default function Page() {
+  return (
+    <html>
+      <body>
+        <main>
+          <h1>Bom Dia</h1>
+        </main>
+      </body>
+    </html>
+  );
+}
